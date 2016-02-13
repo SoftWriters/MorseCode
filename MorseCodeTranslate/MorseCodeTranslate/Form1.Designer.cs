270f1531584cf59c312b62de24b02ec2e8676112
kumitch <kurt.mitchell@outlook.com>
2016-02-13 12:35:13 -0500

    Requested Application 2/13/2016 : Kurt Mitchell
    This is a standard windows forms application that reads a text file and
    converts the international morse code into english characters.

---
+++ MorseCodeTranslate/MorseCodeTranslate/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace MorseCodeTranslate
+{
+    /// <summary>
+    /// Form design class.
+    /// </summary>
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">True if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.lblLocation = new System.Windows.Forms.Label();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.txtFileLocation = new System.Windows.Forms.TextBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.Location = new System.Drawing.Point(337, 111);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(75, 24);
+            this.btnTranslate.TabIndex = 0;
+            this.btnTranslate.Text = "Translate";
+            this.btnTranslate.UseVisualStyleBackColor = true;
+            this.btnTranslate.Click += new System.EventHandler(this.btnTranslate_Click);
+            // 
+            // lblLocation
+            // 
+            this.lblLocation.AutoSize = true;
+            this.lblLocation.Location = new System.Drawing.Point(45, 13);
+            this.lblLocation.Name = "lblLocation";
+            this.lblLocation.Size = new System.Drawing.Size(67, 13);
+            this.lblLocation.TabIndex = 1;
+            this.lblLocation.Text = "File Location";
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(44, 57);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(81, 13);
+            this.lblResult.TabIndex = 1;
+            this.lblResult.Text = "Result will display here";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(337, 31);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(75, 23);
+            this.btnOpen.TabIndex = 5;
+            this.btnOpen.Text = "Browse";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // txtFileLocation
+            // 
+            this.txtFileLocation.Location = new System.Drawing.Point(47, 31);
+            this.txtFileLocation.Name = "txtFileLocation";
+            this.txtFileLocation.Size = new System.Drawing.Size(270, 20);
+            this.txtFileLocation.TabIndex = 6;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(337, 141);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(433, 185);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.txtFileLocation);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.lblLocation);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnTranslate);
+            this.Name = "Form1";
+            this.Text = "Import Users";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label lblLocation;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.TextBox txtFileLocation;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
---
+++ MorseCodeTranslate/MorseCodeTranslate/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MorseCodeTranslate
+{
+    /// <summary>
+    /// Main translation logic.
+    /// </summary>
+    public partial class Form1 : Form
+    {
+        string fileName = string.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Form1" /> class.
+        /// </summary>
+        public Form1()
+        {
+            InitializeComponent();
+            txtFileLocation.Text = string.Empty;
+        }
+
+        /// <summary>
+        /// Main translation.
+        /// </summary>
+        /// <param name="sender">Object invoked event.</param>
+        /// <param name="e">Additional details.</param>
+        private void btnTranslate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtFileLocation.Text == string.Empty)
+                {
+                    MessageBox.Show("A File is Required");
+                    return;
+                }
+
+                var sb = new StringBuilder();
+
+                Dictionary<string, char> morseCode = new Dictionary<string, char>()
+                {
+                    ////Letters
+                    {".-", 'a'},
+                    {"-...", 'b'},
+                    {"-.-.", 'c'},
+                    {"-..", 'd'},
+                    {".", 'e'},
+                    {"..-.", 'f'},
+                    {"--.", 'g'},
+                    {"....", 'h'},
+                    {"..", 'i'},
+                    {".---", 'j'},
+                    {"-.-", 'k'},
+                    {".-..", 'l'},
+                    {"--", 'm'},
+                    {"-.", 'n'},
+                    {"---", 'o'},
+                    {".--.", 'p'},
+                    {"--.-", 'q'},
+                    {".-.", 'r'},
+                    {"...", 's'},
+                    {"-", 't'},
+                    {"..-", 'u'},
+                    {"...-", 'v'},
+                    {".--", 'w'},
+                    {"-..-", 'x'},
+                    {"-.--", 'y'},
+                    {"--..", 'z'},
+                    {" ", ' '},
+
+                    ////Numbers
+                    {"-----", '0'},
+                    {".----", '1'},
+                    {"..----", '2'},
+                    {"...--", '3'},
+                    {"....-", '4'},
+                    {".....", '5'},
+                    {"-....", '6'},
+                    {"--...", '7'},
+                    {"---..", '8'},
+                    {"----.", '9'},
+                };
+
+                ////Builds the string (txtFileLocation)
+                sb.Append(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, txtFileLocation.Text.ToString())));
+
+                ////Removes breaks and blank lines
+                string str = sb.Replace("||||", "|| ||")
+                               .Replace("\r\n", "||")
+                               .ToString();
+
+                char morseResult;
+
+                ////Splits the string into an array based on the pipe breaks.
+                string[] entryArray = Regex.Split(str, @"\|\|");
+                sb.Clear();
+
+                ////Rebuilds the string into deliverable format.
+                foreach (var entry in entryArray)
+                {
+                    if (morseCode.TryGetValue(entry, out morseResult))
+                    {
+                        sb.Append(morseResult);
+                    }
+                    else if (entry == string.Empty)
+                    {
+                        sb.Append("\n\n");
+                    }
+                    else
+                    {
+                        throw new Exception("Please enter a valid Code/File\n\nUse '||' between each letter/number and '||||' between each word\n\n");
+                    }
+                }
+
+                ////Displays the results in the form window
+                lblResult.Text = sb.ToString();
+
+                ////Console Output
+                Console.WriteLine();
+                Console.WriteLine(sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            fileName = string.Empty;
+        }
+
+        /// <summary>
+        /// Opens dialogue for text file input.
+        /// </summary>
+        /// <param name="sender">Object invoked event.</param>
+        /// <param name="e">Additional details.</param>
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                fileName = openFileDialog1.FileName;
+                txtFileLocation.Text = fileName;
+            }
+        }
+
+        /// <summary>
+        /// Closes translator.
+        /// </summary>
+        /// <param name="sender">Object invoked event.</param>
+        /// <param name="e">Additional details.</param>
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
