be8abf823fa2b180744d76668d54c55e84924db5
Elijahjohnson5 <elijahjohnson987@gmail.com>
2019-03-22 15:24:45 -0600

    Started the converter

---
+++ MorseCode/MorseCodeToEnglishTranslator.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MorseCode
+{
+    public class MorseCodeToEnglishTranslator : IOneWayTranslator
+    {
+        private IDictionary<string, char> morseToCharDict;
+
+        public MorseCodeToEnglishTranslator()
+        {
+            morseToCharDict = new Dictionary<string, char>
+            {
+                { ".-", 'a' },
+                { "-...", 'b' },
+                { "-.-.", 'c' },
+                { "-..", 'd' },
+                { ".", 'e' },
+                { "..-.", 'f' },
+                { "--.", 'g' },
+                { "....", 'h' },
+                { "..", 'i' },
+                { ".---", 'j' },
+                { "-.-", 'k' },
+                { ".-..", 'l' },
+                { "--", 'm' },
+                { "-.", 'n' },
+                { "---", 'o' },
+                { ".--.", 'p' },
+                { "--.-", 'q' },
+                { ".-.", 'r' },
+                { "...", 's' },
+                { "-", 't' },
+                { "..-", 'u' },
+                { "...-", 'v' },
+                { ".--", 'w' },
+                { "-..-", 'x' },
+                { "-.--", 'y' },
+                { "--..", 'z' },
+                { "-----", '0' },
+                { ".----", '1' },
+                { "..---", '2' },
+                { "...--", '3' },
+                { "....-", '4' },
+                { ".....", '5' },
+                { "-....", '6' },
+                { "--...", '7' },
+                { "---..", '8' },
+                { "----.", '9' }
+            };
+        }
+
+        public IList<string> TranslateFile(string file)
+        {
+            IList<string> result = new List<string>();
+            string[] lines = File.ReadAllLines(file);
+            foreach (string line in lines)
+            {
+                result.Add(TranslateLine(line));
+            }
+
+            return result;
+        }
+
+        public string TranslateLine(string line)
+        {
+            string[] split = line.Split(new string[] { "||" }, StringSplitOptions.None);
+            StringBuilder sb = new StringBuilder();
+            foreach (string s in split)
+            {
+                if (s.Length == 0)
+                {
+                    sb.Append(" ");
+                } else if (morseToCharDict.ContainsKey(s))
+                {
+                    sb.Append(morseToCharDict[s]);
+                } else
+                {
+                    sb.Append('*');
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ MorseCode/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MorseCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("Program takes one argument that is a file of morse code to translate");
+                Console.WriteLine("Example usage: java MorseCode.class test.txt");
+                return;
+            } else if (!File.Exists(args[0]))
+            {
+                Console.WriteLine("Could not find specified file " + args[0]);
+                Console.WriteLine("Make sure the file path and name is correct");
+                return;
+            }
+
+            IOneWayTranslator translator = new MorseCodeToEnglishTranslator();
+            Console.WriteLine("Translating file " + args[0] + " to english");
+            IList<String> lines = translator.TranslateFile(args[0]);
+            //Print out translated lines one by one
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
